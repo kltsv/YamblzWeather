36f072f8a579e2094c8d0a43cd26dcf217b6ab1b
3DRing <45933861qw>
2017-07-07 13:55:34 +0500

    setting up structure for navigation drawer

--- app/src/main/java/com/ringov/yamblzweather/MainActivity.java
+++ app/src/main/java/com/ringov/yamblzweather/MainActivity.java
@@ -13,6 +13,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.main_activity);
     }
 }
